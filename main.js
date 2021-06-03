ab0e1dfb523af083ed1d1f1349cfae260970e14d
Anne Golladay <annegolladay@gmail.com>
2021-06-03 13:58:45 -0600

    Saving all work

--- main.js
+++ main.js
@@ -8,12 +8,18 @@ function residentFun() {
     console.log('Button clicked')
     axios
     .get('http://swapi.dev/api/planets/?search=Alderaan')
+    
     .then(function(res) {
+        //console.log(res)
         for(let i = 0; i < res.data.results[0].residents.length; i++) {
             let url = res.data.results[0].residents[i]
             axios
             .get(url)
-            .then(function())
+            .then(function(res) {
+                let hTwoEl = document.createElement('h2')
+                hTwoEl.textContent = res.data.name
+                document.querySelector('body').appendChild(hTwoEl)
+            })
         }
     })
 }
@@ -25,6 +31,15 @@ residentBtn.addEventListener('click', residentFun)
 
 // 4. Open index.html in the browser (right click and copy path)
 
-
-
 // 5. Click the button and check the console, if it’s working, move on to the next section
+
+//making a request
+// Now you’ll modify the function to make an axios call to SWAPI
+
+// Make an axios request that gets the information about the planet Alderaan (use the search query to request it, the how to on the search query is at the bottom of the Getting Started section of the documentation)
+
+// Inside the callback passed to the .then, loop over the residents array returned on the results. It’s full of URLs.
+
+// In the loop, make another get request for each URL in the array.
+
+// You’ll have another .then that has its own callback, inside which you should create an h2 element whose content is the name of the resident that you just requested. Append the h2 to your HTML document.
